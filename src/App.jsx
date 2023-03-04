853e952fd18d2971af4da83ce84591f17ad09db6
AbdulMoeez21 <moizrao7@gmail.com>
2023-03-05 00:16:37 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import FormInput from "./components/FormInput";
@@ -23,14 +23,17 @@ function App() {
       placeholder: "Username",
       errorMessage:"Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
+      pattern: "^[A-za-z0-9]{3,16}$",
+      required: true
     },
     {
       id: 2,
       name: "email",
-      type: "text",
+      type: "email",
       placeholder: "Email",
       errorMessage:"It should be a valid email address!",
       label: "Email",
+      required: true
     },
     {
       id: 3,
@@ -47,6 +50,8 @@ function App() {
       placeholder: "Password",
       errorMessage:"Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
+      pattern :`^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true
     },
     {
       id: 5,
@@ -55,6 +60,8 @@ function App() {
       placeholder: "Confirm Password",
       errorMessage:"Passwords don't match!",
       label: "Confirm Password",
+      pattern: values.password,
+      required: true
     },
   ];
 
@@ -67,7 +74,8 @@ function App() {
  const onChange=(e)=>{
   setValues({...values,[e.target.name]: e.target.value})
  }
-console.log(values)
+
+// console.log(values)
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
@@ -78,13 +86,13 @@ console.log(values)
         <FormInput name="email" placeholder="Email" />
         <FormInput name="fullname"  placeholder="Full Name" />
         <FormInput  name="password" placeholder="Password" /> */}
-     {inputs.map(input=>(
-      <FormInput key={input.id} {...input} value={values[input.name]} onChange={onChange}/>
-     ))
-
-     }
-       
-        <button>Submit</button>
+     {inputs.map((input)=>(
+      <FormInput key={input.id} 
+      {...input}
+       value={values[input.name]} 
+       onChange={onChange}/>
+     ))}
+        <button >Submit</button>
       </form>
     </div>
   );
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 import "./FormInput.css";
 
 const FormInput = (props) => {
-const {label,onChange,errorMessage,id,...inputProps}=props
+  const [focused,setFocused]=useState(false)
+const {label,errorMessage,onChange,id,...inputProps}=props 
+
+const handleFocus=(e)=>{
+  setFocused(true)
+}
 
   return (
     <div className="formInput">
-      <label>Username</label>
+      {/* <label>Username</label> */}
       {/* <input
         placeholder={props.placeholder}
         onChange={(e) => props.setUsername(e.target.value)} />*/}
         {/* <input ref={props.refer} placeholder={props.placeholder}/> */}
         {/* <input name={props.name} placeholder={props.placeholder}/>  */}
         <label>{label}</label>
-        <input {...inputProps} onChange={onChange}/>
+        <input {...inputProps} 
+        onChange={onChange}
+         onBlur={handleFocus} 
+         onFocus={()=>
+         inputProps.name==="confirmPassword" && setFocused(true)} 
+         focused={focused.toString()} />
         <span>{errorMessage}</span>
     </div>
   );
